6d0a8acd3d3278bef8169a2f7149791f7cdb9c59
I-the-Creator <andrey.burluckiy@gmail.com>
2021-07-03 17:22:09 +0200

    jquery added

--- dz_5/js/main.js
+++ dz_5/js/main.js
@@ -17,10 +17,10 @@ function doMagic() {
     var btn = document.querySelector('.btn');   //  обращение по классу или другому селектору
     btn.onclick = function () {
         //let ul = document.querySelector('ul');
-        document.querySelector('ul').classList.toggle('menu-clicked'); // вызов без объявления переенной
+        document.querySelector('ul').classList.toggle('menu-clicked'); // вызов без объявления переенной, добавление к ul класса 'menu-clicked'
         var lis = document.querySelectorAll('.menu li'); // обращение ко всем li внутри  класса 'menu' и создание массива из них
         for(let i = 0; i < lis.length; i++) {
-        // lis[i].classList.add('red');   //  чтение классов(стандартный метод) у каждого ли и добавление нового класса
+        // lis[i].classList.add('red');   //  чтение классов(стандартный метод) у каждого li и добавление нового класса
             lis[i].classList.toggle('red');   //  то же самое но с переключателем
         // lis[i].classList.remove('red');   //  удаление класса 
         }
@@ -29,7 +29,7 @@ function doMagic() {
 
 
 function something () {
-    var btn = document.getElementById('btn');   // обращение тольео по id
+    var btn = document.getElementById('btn');   // обращение только по id
     btn.onclick = function () {
         alert('Kнопка кликнута');
     }
